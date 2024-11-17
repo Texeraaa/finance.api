73c2d80fe299e58667addd4114e9bb614393d49f
Texeraaa <pedro.henrique.ph2018@hotmail.com>
2024-11-17 03:53:38 -0300

    reset project

--- src/incomes/incomes.controller.ts
+++
@@ -1,4 +0,0 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('incomes')
-export class IncomesController {}
--- src/incomes/incomes.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { IncomesController } from './incomes.controller';
-import { IncomesService } from './incomes.service';
-
-@Module({
-  controllers: [IncomesController],
-  providers: [IncomesService]
-})
-export class IncomesModule {}
